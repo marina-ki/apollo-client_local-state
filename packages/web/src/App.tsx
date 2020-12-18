2472ff7747216052bec54ad3b10b8086347dbd7a
k-810 <k810marina@gmail.com>
2020-12-19 03:40:34 +0900

    writeQueryの例

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useUserQuery, UserDocument } from './generated/graphql';
+import { ApolloClient, InMemoryCache,ApolloProvider } from '@apollo/client';
+
 
 function App() {
+  const {data,client,refetch} = useUserQuery({
+    nextFetchPolicy: "network-only"
+  })
+  const {} =new InMemoryCache()
+  console.warn(data?.users_by_pk);
+  console.warn("activeUserId",data?.activeUserId);
+  useEffect(()=>{
+    client.writeQuery({ query:UserDocument, data:{activeUserId: 1}})
+    refetch()
+  },[])
   return (
     <div className="App">
       <header className="App-header">
